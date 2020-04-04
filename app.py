522cfeb3d690c229a549ffe9383758a13831ba7f
Syreniac <SyreniacTEST@gmail.com>
2020-04-04 19:27:04 +0100

    Add login page

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+from config import Config
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
 
 @app.route('/')
+@app.route('/index')
 def index():
 	return "Hello world"
 
-@app.route('/create')
-def cookie_create():
-	return render_template('create.html', name="Create")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
