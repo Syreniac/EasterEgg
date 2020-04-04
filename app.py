d31603730b379242723ac65efabfd11147298e8e
Syreniac <SyreniacTEST@gmail.com>
2020-04-04 19:14:34 +0100

    Add a templated page

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Hello world!"
+	return "Hello world"
+
+@app.route('/create')
+def cookie_create():
+	return render_template('create.html', name="Create")
