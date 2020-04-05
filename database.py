34183b6ef21085285c3343e4e62cd4e43d0d64e1
Syreniac <SyreniacTEST@gmail.com>
2020-04-05 10:31:35 +0100

    Improve user persistence

--- database.py
+++ database.py
@@ -1,10 +1,37 @@
 from pydblite import Base
+from flask_login import UserMixin
 
 db = Base('easteregg')
 
+# Password stored as a hash
 db.create('username','password','eggs', mode='open')
+
+class User(UserMixin):
+	def __init__(self, **kwargs):
+		self.id =  kwargs['__id__'] if '__id__' in kwargs else None
+		self.username = kwargs['username']
+		# Stored as a hash 
+		self.password = kwargs['password']
+		self.eggs = kwargs['eggs']
+		
+	def save(self):
+		try:
+			if self.id is None:
+				self.id = db.insert(username=self.username, password=self.password, eggs=self.eggs)
+			else:
+				db.update(self.id, username=self.username, password=self.password, eggs=self.eggs)
+			db.commit()
+			return true
+		except Exception as E:
+			return false
+
+def load_user(user_id):
+	return User(**db[int(user_id)])
 
 if db.exists:
 	db.open()
 	
-db.insert(username='testuser', password='hunter2', eggs=['cs-tc3-dev100'])
+user = User(username='Test', password='Hunter2', eggs=['cs-tc3-dev100'])
+user.save()
+user = load_user(user.get_id())
+print(user.eggs)
