44dc6281f4febac640ca5c0328b5042cb1b856b7
Syreniac <SyreniacTEST@gmail.com>
2020-04-05 10:51:59 +0100

    Enforce login behaviour

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from flask_login import current_user, login_user, LoginManager
+from database import load_user, find_user_by_username
 
 from config import Config
 
@@ -12,8 +14,8 @@ class LoginForm(FlaskForm):
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-
+login = LoginManager(app)
+login.user_loader(load_user)
 
 @app.route('/')
 @app.route('/index')
@@ -22,9 +24,14 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = find_user_by_username(form.username.data)
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=True)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from pydblite import Base
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = Base('easteregg')
 
@@ -13,6 +14,12 @@ class User(UserMixin):
 		# Stored as a hash 
 		self.password = kwargs['password']
 		self.eggs = kwargs['eggs']
+
+	def set_password(self, password):
+		self.password = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password, password)
 		
 	def save(self):
 		try:
@@ -21,17 +28,23 @@ class User(UserMixin):
 			else:
 				db.update(self.id, username=self.username, password=self.password, eggs=self.eggs)
 			db.commit()
-			return true
+			return True
 		except Exception as E:
-			return false
+			return False
 
 def load_user(user_id):
 	return User(**db[int(user_id)])
+	
+def find_user_by_username(username):
+	for user in db:
+		if user['username'] == username:
+			return User(**user)
+	return None
 
 if db.exists:
 	db.open()
 	
-user = User(username='Test', password='Hunter2', eggs=['cs-tc3-dev100'])
+user = User(username='Test', password=generate_password_hash('Hunter2'), eggs=['cs-tc3-dev100'])
 user.save()
 user = load_user(user.get_id())
 print(user.eggs)
